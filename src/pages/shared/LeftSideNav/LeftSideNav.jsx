b2c34f7c8cbeca5c3fc210d51e57875f09126b59
Md. Mahbub hiron <mhalom19@gmail.com>
2025-01-05 16:29:45 +0600

    both side navbar design done

--- src/pages/shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect( ()=>{
+        fetch('categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
+    
     return (
-        <div>
-            <h2>Left side nav</h2>
+        <div className='space-y-6'>
+            <h2 className='text-2xl'>All Categories</h2>
+            {
+                categories.map(category => <NavLink 
+                    key={category.id} 
+                    className="block ml-4 text-xl font-semibold" to={`/category/${category.id}`}>{category.name}</NavLink>)
+            }
         </div>
     );
 };
--- src/pages/shared/RightSideNav/RightSideNav.jsx
+++ src/pages/shared/RightSideNav/RightSideNav.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+// import {  } from "react-icons/fa";
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram  } from "react-icons/fa6";
+import swimming from '../../../assets/swimming.png';
+import classImg from '../../../assets/class.png';
+import playground from '../../../assets/playground.png';
 
 const RightSideNav = () => {
-    return (
-        <div>
-            <h2>Right side nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="p-4 space-y-4 mb-4">
+        <h2 className="text-2xl font-bold">Login With</h2>
+        <button className="btn btn-outline w-full">
+        <FaGoogle />
+          Login With Google
+        </button>
+        <button className="btn btn-outline w-full">
+        <FaGithub />
+          Login With GitHub
+        </button>
+      </div>
+      <div className="p-4 mb-4">
+        <h2 className="text-2xl mb-6 font-bold">Find Us On</h2>
+        <a className="p-4 flex items-center gap-3 text-lg border rounded-t-lg" href="">
+            <FaFacebook></FaFacebook>
+            Facebook
+        </a>
+        <a className="p-4 flex items-center gap-2 text-lg border-x" href="">
+            <FaTwitter></FaTwitter>
+            Twitter
+        </a>
+        <a className="p-4 flex items-center gap-2 text-lg border rounded-b-lg" href="">
+            <FaInstagram></FaInstagram>
+            Instagram
+        </a>
+      </div>
+      <div className="p-4 space-y-3 mb-4">
+        <h2 className="text-3xl font-bold">Q Zone</h2>
+        <img src={swimming} alt="" />
+        <img src={classImg} alt="" />
+        <img src={playground} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
